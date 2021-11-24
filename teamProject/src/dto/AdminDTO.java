99fe131707f46841caebb6e5095e382e11a7cfb1
Junku <64190196+yjk5591@users.noreply.github.com>
2021-11-24 21:09:23 +0900

    AdminDTO confirm(phoneNum parseInt)

--- teamProject/src/dto/AdminDTO.java
+++ teamProject/src/dto/AdminDTO.java
@@ -5,12 +5,12 @@ public class AdminDTO {
 	public String adm_pw;
 	public String adm_name;
 	public int adm_number;
-	public String adm_phonenum;
+	public int adm_phonenum;
 
 	public AdminDTO() {
 	}
 
-	public AdminDTO(String adm_id, String adm_pw, String adm_name, int adm_number, String adm_phonenum) {
+	public AdminDTO(String adm_id, String adm_pw, String adm_name, int adm_number, int adm_phonenum) {
 		this.adm_id = adm_id;
 		this.adm_pw = adm_pw;
 		this.adm_name = adm_name;
@@ -23,7 +23,7 @@ public class AdminDTO {
 		this.adm_pw = datas[1];
 		this.adm_name = datas[2];
 		this.adm_number = Integer.parseInt(datas[3]);
-		this.adm_phonenum = datas[4];
+		this.adm_phonenum = Integer.parseInt(datas[4]);
 	}
 
 	@Override
