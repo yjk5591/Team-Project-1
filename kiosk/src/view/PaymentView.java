6bf2605a9ea5f60dc2515b850307ccfbeebc2bfd
권민주(A01) <alswn5790@naver.com>
2021-11-25 09:52:04 +0900

    1

---
+++ kiosk/src/view/PaymentView.java
@@ -0,0 +1,43 @@
+package view;
+
+import java.util.Scanner;
+
+public class PaymentView {
+	public static void main(String[] args) {
+//		if(Session.get("loginUser") != null) {
+//			Scanner sc = new Scanner(System.in);
+//			UserDTO loginUser = (UserDTO)Session.get("loginUser");
+//			ProductDAO pdao = new ProductDAO();
+//			
+//			System.out.println("========== 장바구니 ==========");
+//			while(true) {
+//				System.out.println("0. 처음으로 돌아가기\n1. 수량 변경\n2. 메뉴 삭제\n3. 쿠폰 사용\n4. 포인트 사용"
+//						+ "\n5. 매장 선택\n6. 배송지 선택");
+//				int choice = sc.nextInt();
+//				if(choice == 0) {
+//					//처음으로 돌아가기
+//					break;
+//				} else if(choice == 1) {
+//					//수량 변경
+////					new ModifyView();
+//				} else if(choice == 2) {
+//					//메뉴 삭제
+////					new ModifyView();
+//				} else if(choice == 3) {
+//					//쿠폰 사용
+////					new PayView();
+//				} else if(choice == 4) {
+//					//포인트 사용
+////					new PayView();
+//				} else if(choice == 5) {
+//					//매장 선택
+//				} else if(choice == 6) {
+//					//배송지 선택
+//				} else {
+//					System.out.println("잘못 입력했습니다. 장바구니로 돌아갑니다.");
+//				}
+//			}
+//			new Index();
+//		}
+	}
+}
