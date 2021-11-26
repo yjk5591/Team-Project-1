f7e54ec2e11fa0c240d643559152884ca2fa7c8d
5YoonCheol <oyc1230@gmail.com>
2021-11-26 23:10:47 +0900

    11-26일자

---
+++ teamProject/src/dto/AdminDTO.java
@@ -0,0 +1,34 @@
+package dto;
+
+public class AdminDTO {
+	public String admin_id;
+	public String admin_pw;
+	public String admin_name;
+	public int admin_num;
+	public String admin_phone;
+	public String admin_store_id;
+	
+	public AdminDTO() {}
+
+	
+	
+	public AdminDTO(String admin_id, String admin_pw, String admin_name, int admin_num, String admin_phone,
+			String admin_store_id) {
+	
+		this.admin_id = admin_id;
+		this.admin_pw = admin_pw;
+		this.admin_name = admin_name;
+		this.admin_num = admin_num;
+		this.admin_phone = admin_phone;
+		this.admin_store_id = admin_store_id;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return admin_id+"\t"+admin_pw+"\t"+admin_name+"\t"+admin_num+"\t"+admin_phone+"\t"+admin_store_id;
+	}
+	
+	
+}
---
+++ teamProject/src/view/AdminJoinView.java
@@ -0,0 +1,65 @@
+package view;
+
+import java.util.Scanner;
+
+import dao.AdminDAO;
+import dto.AdminDTO;
+
+
+
+
+public class AdminJoinView {
+	
+
+	public AdminJoinView() {
+		Scanner sc = new Scanner(System.in);
+		//새로 가입되는 "관리자의 정보들"을 데이터베이스에 저장해야 하므로
+		//데이터베이스에 접근하기 위한(기능을 구현해 놓은) DAO가 필요하고
+		//회원정보에 접근해야 하기 때문에 AdminDAO 객체가 필요하다.
+		AdminDAO adao = new AdminDAO();
+		System.out.println("ID : ");
+		String admin_id = sc.next();
+		String admin_pw = "";
+		if(adao.checkId(admin_id)) {
+			while(true) {
+					System.out.println("PW :");
+					admin_pw = sc.next();
+					System.out.println("PW 확인 :");
+					String admin_pw_re = sc.next();
+					//비밀번호 검사는 따로 데이터베이스를 거치지 않고 뷰단에서 해결
+					if(admin_pw.equals(admin_pw_re)) {
+						break;
+					}else {
+						System.out.println("비밀번호를  확인해주세요.");
+					}
+				}		
+			System.out.println("이름 : ");
+			String admin_name = sc.next();
+			System.out.println("관리자 번호 : ");
+			int admin_num = sc.nextInt();
+			System.out.println("핸드폰 번호 : ");
+			String admin_phone = sc.next();
+			System.out.println("매장 ID : ");
+			String admin_store_id = sc.next();
+			if(adao.checkStore(admin_store_id)) {
+				System.out.println("매장 ID가 정상적으로 등록되었습니다.");
+			}else {
+				System.out.println("중복된 매장ID가 있습니다.");
+			}
+			//DB에 저장될 관리자의 정보를 입력받았다.
+			//AdminDTO 객체를 생성하여 한 번에 넘겨주어 DB로 이동시킨다.
+		
+		AdminDTO newAdmin = new AdminDTO(admin_id, admin_pw, admin_name, admin_num, admin_phone, admin_store_id);
+		if(adao.join(newAdmin)) {
+			System.out.println("회원가입 성공!");
+			System.out.println(admin_name + "님 회원가입을 환영합니다!!");
+		}else {
+			System.out.println("회원가입에 실패했습니다. 다시 시도해주세요.");
+		}
+	}else {
+		System.out.println("중복된 아이디가 있습니다.");
+	}
+		
+	}
+	
+}
