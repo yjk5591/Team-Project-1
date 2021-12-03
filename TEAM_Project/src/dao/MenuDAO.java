bfa02f3e73c55c522f7c1e3aadec78731ed515c0
5YoonCheol <oyc1230@gmail.com>
2021-12-03 23:32:18 +0900

    메뉴 수정,삭제 부분 (완성 및 주석 추가)

--- TEAM_Project/src/dao/MenuDAO.java
+++ TEAM_Project/src/dao/MenuDAO.java
@@ -52,19 +52,49 @@ public class MenuDAO {
 		return result == 1;
 	}
 	public boolean menu_delete(String menu_name) {
+		//메뉴 이름이 menu_name인 행의 정보를 모두 삭제해라 라는 쿼리문 작성
 		String sql = "DELETE FROM MENU_DB WHERE MENU_NAME = ?";
 		int result = 0;
 		try {
+			//담아주고 
 			ps = conn.prepareStatement(sql);
+			//쿼리문의 ?에 menu_name을 입력
 			ps.setString(1, menu_name);
-			
+			//결과는 삭제한 정보가 있다면 1, 없으면 0일 것이다.
 			result = ps.executeUpdate();
 			
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		//그렇기 때문에 삭제에 성공했다면 result값은 참, 실패했다면 거짓이 리턴된다.
 		return result == 1;
 		
+	}
+	
+	public boolean menu_mod(String menu_name, String mod_menu_name, int mod_menu_price) {
+		//메뉴 이름이 menu_name인 행의 메뉴 이름과 메뉴 가격을 수정하는 쿼리문을 작성
+		String sql = "UPDATE MENU_DB SET MENU_NAME = ?, MENU_PRICE = ? WHERE MENU_NAME = ?";
+		int result = 0;
+		try {
+			//담아주고
+			ps = conn.prepareStatement(sql);
+			//쿼리문의 3개의 ? 중 첫 번째 ?에 새로운 메뉴이름을 입력
+			ps.setString(1, mod_menu_name);
+			//두 번째 ?에 새로운 가격 입력
+			ps.setInt(2, mod_menu_price);
+			//세 번째 ?에 기존 메뉴 이름을 입력
+			ps.setString(3, menu_name);
+			//result 값에 수정된 정보가 있다면 1이 담길 것이고, 수정된 정보가 없다면 0이 담길 것이다.
+			result = ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//따라서 수정에 성공했다면 result값은 true, 실패했다면 false가 리턴된다.
+		//또한 수정에 실패했다는 뜻은 기존 MENU_NAME 컬럼에 쿼리문의 조건에 만족하는 정보가 입력되지 않았음을 의미한다.
+		//이는 즉, 입력한 기존 MENU_NAME 값이 잘못되었다는 것을 의미하므로 거짓이 리턴되었다면 
+		//정확한 메뉴이름을 다시 입력해주어야 한다.
+		return result == 1;
 	}
 }
